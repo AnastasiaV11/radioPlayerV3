67c2cbdfb18761d83eeda126ceaa20bd2f0b8c32
AnastasiaV11 <89535376+AnastasiaV11@users.noreply.github.com>
2021-09-02 11:13:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
-cd /RadioPlayerV3
+git clone https://github.com/AnastasiaV11/radioPlayerV3.git /radioPlayerV3
+cd /radioPlayerV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
